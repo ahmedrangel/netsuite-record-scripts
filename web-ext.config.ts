064045e97c52ec184ba687897310733e43a6f0f7
Ahmed Rangel <ahmedrangel@outlook.com>
2025-03-19 20:32:13 -0500

    chore: set web-ext config

---
+++ web-ext.config.ts
@@ -0,0 +1,7 @@
+import { defineRunnerConfig } from "wxt";
+import { resolve } from "node:path";
+
+export default defineRunnerConfig({
+  chromiumProfile: resolve(".wxt/chrome-data"),
+  keepProfileChanges: true
+});
