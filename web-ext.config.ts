01fd8911540769fe895f7d21d12892901b2d72b8
Ahmed Rangel <ahmedrangel@outlook.com>
2025-03-20 19:24:34 -0500

    fix: ensure profile exists

--- web-ext.config.ts
+++ web-ext.config.ts
@@ -1,5 +1,7 @@
 import { defineRunnerConfig } from "wxt";
 import { resolve } from "node:path";
+import { mkdir, readdir } from "node:fs/promises";
+await readdir(".wxt/chrome-data").catch(() => mkdir(".wxt/chrome-data"));
 
 export default defineRunnerConfig({
   chromiumProfile: resolve(".wxt/chrome-data"),
