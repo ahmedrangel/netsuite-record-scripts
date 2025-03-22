8c18df42819476d2bf4140155b3261a816437fce
Ahmed Rangel <ahmedrangel@outlook.com>
2025-03-22 03:00:30 -0500

    fix: web-ext config

--- web-ext.config.ts
+++ web-ext.config.ts
@@ -2,6 +2,7 @@ import { resolve } from "node:path";
 import { mkdir, readdir } from "node:fs/promises";
 import { defineRunnerConfig } from "wxt";
 
+await readdir(".wxt").catch(() => mkdir(".wxt"));
 await readdir(".wxt/chrome-data").catch(() => mkdir(".wxt/chrome-data"));
 
 export default defineRunnerConfig({
