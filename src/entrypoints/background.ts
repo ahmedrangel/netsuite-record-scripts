854f4fa01b168a860433519d38b8c61e3bb6307e
Ahmed Rangel <ahmedrangel@outlook.com>
2025-03-24 19:14:18 -0500

    choe: lint

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,13 +1,15 @@
 export default defineBackground(() => {
-  browser.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
-    if (changeInfo.url) {
-      const url = new URL(changeInfo.url);
-      if (!url.hostname.includes(".netsuite.com")) {
-        await browser.action.disable(tabId);
-        return;
+  browser.tabs.onUpdated.addListener((tabId, changeInfo) => {
+    (async () => {
+      if (changeInfo.url) {
+        const url = new URL(changeInfo.url);
+        if (!url.hostname.includes(".netsuite.com")) {
+          await browser.action.disable(tabId);
+          return;
+        }
+        await browser.action.enable(tabId);
+        await browser.action.setPopup({ popup: "popup-ext.html" });
       }
-      await browser.action.enable(tabId);
-      await browser.action.setPopup({ popup: "popup-ext.html" });
-    }
+    })().then(() => void 0).catch(console.info);
   });
 });
