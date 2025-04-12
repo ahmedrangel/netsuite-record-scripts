471f7dd8824285584287b3344a0c6123a8786128
Ahmed Rangel <ahmedrangel@outlook.com>
2025-04-12 11:42:10 -0500

    refactor: adjust background events code

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -2,29 +2,20 @@ import { getCurrentTabId, handlePopup } from "@/utils/helpers";
 
 export default defineBackground(() => {
   browser.runtime.onInstalled.addListener(() => {
-    (async () => {
-      const tabId = await getCurrentTabId();
-      await handlePopup(tabId);
-    })().then(() => void 0).catch(console.info);
+    getCurrentTabId().then((tabId) => handlePopup(tabId)).then(() => void 0).catch(console.info);
   });
 
   browser.tabs.onActivated.addListener(({ tabId }) => {
-    (async () => {
-      await handlePopup(tabId);
-    })().then(() => void 0).catch(console.info);
+    handlePopup(tabId).then(() => void 0).catch(console.info);
   });
 
   browser.tabs.onUpdated.addListener((tabId, changeInfo) => {
-    (async () => {
-      if (changeInfo.status === "complete") await handlePopup(tabId);
-    })().then(() => void 0).catch(console.info);
+    if (changeInfo.status !== "complete") return;
+    handlePopup(tabId).then(() => void 0).catch(console.info);
   });
 
   browser.windows.onFocusChanged.addListener((windowId) => {
-    (async () => {
-      if (windowId === browser.windows.WINDOW_ID_NONE) return;
-      const tabId = await getCurrentTabId();
-      await handlePopup(tabId);
-    })().then(() => void 0).catch(console.info);
+    if (windowId === browser.windows.WINDOW_ID_NONE) return;
+    getCurrentTabId().then((tabId) => handlePopup(tabId)).then(() => void 0).catch(console.info);
   });
 });
