c344958969e590b3fbb59f095202a6b0ee5ca231
Ahmed Rangel <ahmedrangel@outlook.com>
2025-04-22 16:26:24 -0500

    chore: remove unnecessary then void

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -2,20 +2,20 @@ import { getCurrentTabId, handlePopup } from "@/utils/helpers";
 
 export default defineBackground(() => {
   browser.runtime.onInstalled.addListener(() => {
-    getCurrentTabId().then((tabId) => handlePopup(tabId)).then(() => void 0).catch(console.info);
+    getCurrentTabId().then((tabId) => handlePopup(tabId)).catch(console.info);
   });
 
   browser.tabs.onActivated.addListener(({ tabId }) => {
-    handlePopup(tabId).then(() => void 0).catch(console.info);
+    handlePopup(tabId).catch(console.info);
   });
 
   browser.tabs.onUpdated.addListener((tabId, changeInfo) => {
     if (changeInfo.status !== "complete") return;
-    handlePopup(tabId).then(() => void 0).catch(console.info);
+    handlePopup(tabId).catch(console.info);
   });
 
   browser.windows.onFocusChanged.addListener((windowId) => {
     if (windowId === browser.windows.WINDOW_ID_NONE) return;
-    getCurrentTabId().then((tabId) => handlePopup(tabId)).then(() => void 0).catch(console.info);
+    getCurrentTabId().then((tabId) => handlePopup(tabId)).catch(console.info);
   });
 });
