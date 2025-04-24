525a2bcb4892b0c98051eae51fc3a6a0e306e3b8
Ahmed Rangel <ahmedrangel@outlook.com>
2025-04-23 22:59:58 -0500

    chore(helpers): directly return mapped objects

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,54 +2,48 @@ import { load } from "cheerio";
 
 export const getScripts = (html: string): NetSuiteScript[][] => {
   const $ = load(html);
-  const userEvent = $("[id^=\"serverrow\"]").map((_, el): NetSuiteScript => {
-    return {
-      type: "userevent",
-      name: $(el).find("td:nth-child(2)").text(),
-      url: $(el).find("td:nth-child(2) > a").attr("href"),
-      owner: $(el).find("td:nth-child(3)").text(),
-      ownerUrl: $(el).find("td:nth-child(3) > a").attr("href"),
-      version: $(el).find("td:nth-child(4)").text(),
-      status: $(el).find("td:nth-child(8) * input").attr("value"),
-      functions: {
-        beforeLoad: $(el).find("td:nth-child(10)").text(),
-        beforeSubmit: $(el).find("td:nth-child(11)").text(),
-        afterSubmit: $(el).find("td:nth-child(12)").text()
-      }
-    };
-  }).get();
+  const userEvent = $("[id^=\"serverrow\"]").map((_, el): NetSuiteScript => ({
+    type: "userevent",
+    name: $(el).find("td:nth-child(2)").text(),
+    url: $(el).find("td:nth-child(2) > a").attr("href"),
+    owner: $(el).find("td:nth-child(3)").text(),
+    ownerUrl: $(el).find("td:nth-child(3) > a").attr("href"),
+    version: $(el).find("td:nth-child(4)").text(),
+    status: $(el).find("td:nth-child(8) * input").attr("value"),
+    functions: {
+      beforeLoad: $(el).find("td:nth-child(10)").text(),
+      beforeSubmit: $(el).find("td:nth-child(11)").text(),
+      afterSubmit: $(el).find("td:nth-child(12)").text()
+    }
+  })).get();
 
-  const client = $("[id^=\"clientrow\"]").map((_, el): NetSuiteScript => {
-    return {
-      type: "client",
-      name: $(el).find("td:nth-child(2)").text(),
-      url: $(el).find("td:nth-child(2) > a").attr("href"),
-      owner: $(el).find("td:nth-child(3)").text(),
-      ownerUrl: $(el).find("td:nth-child(3) > a").attr("href"),
-      version: $(el).find("td:nth-child(4)").text(),
-      status: $(el).find("td:nth-child(8) * input").attr("value"),
-      functions: {
-        pageInit: $(el).find("td:nth-child(10)").text(),
-        saveRecord: $(el).find("td:nth-child(11)").text(),
-        fieldChanged: $(el).find("td:nth-child(12)").text(),
-        validateLine: $(el).find("td:nth-child(13)").text()
-      }
-    };
-  }).get();
+  const client = $("[id^=\"clientrow\"]").map((_, el): NetSuiteScript => ({
+    type: "client",
+    name: $(el).find("td:nth-child(2)").text(),
+    url: $(el).find("td:nth-child(2) > a").attr("href"),
+    owner: $(el).find("td:nth-child(3)").text(),
+    ownerUrl: $(el).find("td:nth-child(3) > a").attr("href"),
+    version: $(el).find("td:nth-child(4)").text(),
+    status: $(el).find("td:nth-child(8) * input").attr("value"),
+    functions: {
+      pageInit: $(el).find("td:nth-child(10)").text(),
+      saveRecord: $(el).find("td:nth-child(11)").text(),
+      fieldChanged: $(el).find("td:nth-child(12)").text(),
+      validateLine: $(el).find("td:nth-child(13)").text()
+    }
+  })).get();
 
-  const workflows = $("[id^=\"workflowsrow\"]").map((_, el): NetSuiteScript => {
-    return {
-      type: "workflow",
-      name: $(el).find("td:nth-child(1)").text(),
-      url: $(el).find("td:nth-child(1) > a").attr("href"),
-      owner: $(el).find("td:nth-child(7)").text(),
-      ownerUrl: $(el).find("td:nth-child(7) > a").attr("href"),
-      status: $(el).find("td:nth-child(8) * input").attr("value"),
-      functions: {
-        trigger: $(el).find("td:nth-child(9) * input").attr("value")?.replaceAll("-", "")
-      }
-    };
-  }).get();
+  const workflows = $("[id^=\"workflowsrow\"]").map((_, el): NetSuiteScript => ({
+    type: "workflow",
+    name: $(el).find("td:nth-child(1)").text(),
+    url: $(el).find("td:nth-child(1) > a").attr("href"),
+    owner: $(el).find("td:nth-child(7)").text(),
+    ownerUrl: $(el).find("td:nth-child(7) > a").attr("href"),
+    status: $(el).find("td:nth-child(8) * input").attr("value"),
+    functions: {
+      trigger: $(el).find("td:nth-child(9) * input").attr("value")?.replaceAll("-", "")
+    }
+  })).get();
   return [userEvent, client, workflows];
 };
 
