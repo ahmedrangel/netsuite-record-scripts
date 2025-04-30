3112474b0e387ad947000e40f6b1e358b3f0b4ba
Ahmed Rangel <ahmedrangel@outlook.com>
2025-04-30 18:33:08 -0500

    feat: add custom record button + slightly adjust styles

--- src/components/PopupCard.vue
+++ src/components/PopupCard.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/vue";
 import { Icon } from "@iconify/vue";
+import { getQuery, withQuery } from "ufo";
 import { getCurrentTabId, getScripts } from "../utils/helpers";
 import ScriptPanel from "./ScriptPanel.vue";
 import NetsuiteRecordScriptsIcon from "./NetsuiteRecordScriptsIcon.vue";
@@ -10,6 +11,7 @@ const loading = ref(false);
 const fetched = ref(false);
 const record = ref("");
 const netsuiteOrigin = ref("");
+const recType = ref<string | null>(null);
 
 const userEventScripts = ref<NetSuiteScript[]>([]);
 const clientScripts = ref<NetSuiteScript[]>([]);
@@ -26,16 +28,18 @@ onMounted(async () => {
       const input = document.querySelector("#baserecordtype") as HTMLInputElement;
       return {
         recordType: input?.value || null,
-        origin: window.location.origin || null
+        origin: window.location.origin || null,
+        location: window.location.href || null
       };
     }
   });
-  const { recordType, origin } = (result[0].result!);
+  const { recordType, origin, location } = (result[0].result!);
   if (!recordType || !origin) {
     fetched.value = true;
     return;
   }
   netsuiteOrigin.value = origin;
+  recType.value = getQuery(location || "")?.rectype as string || null;
   record.value = recordType;
   loading.value = true;
   const requestResult = await browser.scripting.executeScript({
@@ -99,17 +103,25 @@ watchEffect(() => {
       <TabList class="flex align-center justify-center gap-1 pb-1">
         <template v-for="(tab, i) in tabs" :key="i">
           <Tab v-slot="{ selected }" class="w-full rounded overflow-hidden cursor-pointer border border-violet-900" @click="filterInput?.focus()">
-            <div class="flex align-center justify-center gap-1 p-2" :class="selected ? 'bg-violet-500/30' : 'bg-violet-900/70'">
+            <div class="flex align-center justify-center gap-1 p-2" :class="selected ? 'bg-violet-500/30 hover:bg-violet-500/40' : 'bg-violet-900/70 hover:bg-violet-900/82'">
               <span class="text-md font-bold" :class="selected ? '' : 'text-slate-50'">{{ tab.name }}</span>
               <span class="px-1.5 rounded border font-bold bg-lime-200 border-lime-600">{{ tab.count }}</span>
             </div>
           </Tab>
         </template>
       </TabList>
-      <div class="mb-1 flex items-center rounded bg-slate-50 px-1.5 py-1.5 outline-1 -outline-offset-1 outline-violet-900 has-[input:focus-within]:outline-2">
-        <input ref="filterInput" v-model="searchInput" type="text" name="filter" class="block min-w-0 grow pr-3 pl-1 text-xs text-gray-900 placeholder:text-gray-500 focus:outline-none" placeholder="Type to filter...">
-        <div v-if="searchInput" class="grid shrink-0 grid-cols-1 focus-within:relative cursor-pointer" role="button" @click="searchInput = ''">
-          <Icon icon="ph:x-circle-duotone" class="text-rose-600" height="15" width="20" />
+      <div class="flex items-center justify-between mb-1">
+        <div class="flex items-center bg-slate-50 p-1.5 outline-1 -outline-offset-1 outline-violet-900/70 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 w-full rounded-l">
+          <input ref="filterInput" v-model="searchInput" type="text" name="filter" class="block min-w-0 grow pr-3 pl-1 text-xs text-gray-900 placeholder:text-gray-500 focus:outline-none" placeholder="Type to filter...">
+          <div v-if="searchInput" class="grid shrink-0 grid-cols-1 focus-within:relative cursor-pointer" role="button" @click="searchInput = ''">
+            <Icon icon="ph:x-circle-duotone" class="text-rose-600" height="15" width="20" />
+          </div>
+        </div>
+        <div class="text-xs rounded-r">
+          <a v-if="recType" :href="recType ? withQuery(`${netsuiteOrigin}/app/common/custom/custrecord.nl`, { id: recType, e: 'T' }) : ''" target="_blank" rel="noopener noreferrer" class="px-2 py-1.5 bg-violet-900/70 hover:bg-violet-900/82">
+            <span class="text-slate-50 font-normal">{{ record }}</span>
+          </a>
+          <span v-else class="text-slate-50 px-2 py-1.5 bg-violet-900/70">{{ record }}</span>
         </div>
       </div>
       <TabPanels class="relative overflow-hidden">
